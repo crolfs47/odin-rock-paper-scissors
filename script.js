ede224e53228b24843eb221464f83ed3ca57c6d6
Cathy Rolfs <crolfs47@gmail.com>
2022-08-08 11:32:33 -0500

    Add playRound function

--- script.js
+++ script.js
@@ -4,3 +4,47 @@ function getComputerChoice() {
     let randChoice = choice[Math.floor(Math.random() * choice.length)];
     return randChoice;
 }
+
+
+function playRound(playerSelection, computerSelection) {
+    computerSelection = getComputerChoice();
+    playerSelection = prompt("choose rock, paper, or scissors");
+    player = playerSelection.toLowerCase();
+    computer = computerSelection;
+    console.log(player);
+    console.log(computer);
+    if (player == 'rock') {
+        if (computer == 'scissors') {
+            return 1;
+        }
+        else {
+            return 0;
+        }
+    }
+    if (player == 'paper') {
+        if (computer == 'rock') {
+            return 1;
+        }
+        else {
+            return 0;
+        }
+    }
+    if (player == 'scissors') {
+        if (computer == 'paper') {
+            return 1;
+        }
+        else {
+            return 0;
+        }
+    }
+    
+}
+
+if (playRound() == 1) {
+    console.log("Player Wins");
+}
+
+
+    
+
+
