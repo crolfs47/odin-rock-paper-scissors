de9fcf305e1674a788b2ecfb1a5851078c3da648
Cathy Rolfs <crolfs47@gmail.com>
2022-08-22 11:29:18 -0500

    Add end game function, restart game functionality

--- script.js
+++ script.js
@@ -6,14 +6,15 @@ const loss = "Player Loses :(";
 let playerScore = 0;
 let computerScore = 0;
 
+const buttons = document.querySelectorAll('.choiceButton');
 const playerChoice = document.querySelector('#playerSelection');
 const computerChoice = document.querySelector('#computerSelection');
 const results = document.querySelector('#results');
 const playerPoints = document.querySelector('#playerScore');
 const computerPoints = document.querySelector('#computerScore');
 const gameOver = document.querySelector('#gameOver');
+const restart = document.querySelector('#restart');
 
-const buttons = document.querySelectorAll('button');
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
         playRound(button.id,);
@@ -39,91 +40,58 @@ function playRound(playerSelection, computerSelection) {
         if (computer == 'scissors') {
             results.textContent = "Results: " + win;
             playerPoints.textContent = "Player Score: " + ++playerScore;
-            //return win;
-            if (playerScore === 5) {
-                gameOver.textContent = "Game over! Player wins!";
-            }
+            endGame();
         }
         else {
             results.textContent = "Results: " + loss;
             computerPoints.textContent = "Computer Score: " + ++computerScore;
-            //return loss;
-            if (computerScore === 5) {
-                gameOver.textContent = "Game over! Computer wins!";
-            }
+            endGame();
         }
     }
     else if (player == 'paper') {
         if (computer == 'rock') {
             results.textContent = "Results: " + win;
             playerPoints.textContent = "Player Score: " + ++playerScore;
-            //return win;
-            if (playerScore === 5) {
-                gameOver.textContent = "Game over! Player wins!";
-            }
+            endGame();
         }
         else {
             results.textContent = "Results: " + loss;
             computerPoints.textContent = "Computer Score: " + ++computerScore;
-            //return loss;
-            if (computerScore === 5) {
-                gameOver.textContent = "Game over! Computer wins!";
-            }
+            endGame();
         }
     }
     else if (player == 'scissors') {
         if (computer == 'paper') {
             results.textContent = "Results: " + win;
             playerPoints.textContent = "Player Score: " + ++playerScore;
-            //return win;
-            if (playerScore === 5) {
-                gameOver.textContent = "Game over! Player wins!";
-            }
+            endGame();
         }
         else {
             results.textContent = "Results: " + loss;
             computerPoints.textContent = "Computer Score: " + ++computerScore;
-            //return loss;
-            if (computerScore === 5) {
-                gameOver.textContent = "Game over! Computer wins!";
-            }
+            endGame();
         }
     } 
 }
 
+function endGame() {
+    if (playerScore === 5) {
+        gameOver.textContent = "Game over! Player wins!";
+        buttons.forEach((button) => {
+            button.disabled = true;
+        })
+    }
+    else if (computerScore === 5) {
+        gameOver.textContent = "Game over! Computer wins!";
+        buttons.forEach((button) => {
+            button.disabled = true;
+        })  
+}}
 
+restart.addEventListener('click', () => {
+    location.reload();
+})
 
-
-
-
-
-
-
-//function game() {
-//    playerScore = 0;
-//    computerScore = 0;
-
-//    for (let i = 0; i < 5; i++) {
-//        result = playRound();
-//        if (result == win) {
-//            playerScore += 1;
-//       }
-//        if (result == loss) {
-//            computerScore += 1;
-//        }
-//        console.log("Player Score = " + playerScore + ", Computer Score = " + computerScore);
-//    }
-
-//    if (playerScore > computerScore) {
-//        console.log("Player Wins!");
-//    }
-//    else if (playerScore < computerScore) {
-//        console.log("Computer Wins!");
-//    }
-//    else {
-//        console.log("Tie!");
-//    }
-//}
 
 
 
